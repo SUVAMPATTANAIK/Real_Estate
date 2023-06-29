0f2496d98e3767b7f5e2ca5e81920b6d3d4d863d
Suvam <pattanaiksuvam2001@gmail.com>
2023-06-29 16:44:48 +0530

    First commit

---
+++ src/components/Header/Header.jsx
@@ -0,0 +1,38 @@
+import React,{ useState} from 'react'
+import './Header.css'
+import {BiMenuAltRight} from 'react-icons/bi'
+const Header = () => {
+    const [menuOpened,setMenuOpened]=useState(false)
+
+    const getMenuStyles=(menuOpened)=>{
+        if(document.documentElement.clientWidth<=800
+            ){
+                return {right:!menuOpened && "-100%"}
+            }
+    }
+    return (
+        <section className='h-wrapper'>
+            <div className="flexCenter paddings innerWidth h-container">
+                <img src="./logo.png" alt="logo" width={100} />
+
+                <div className="flexCenter h-menu" style={getMenuStyles(menuOpened)}>
+                    
+                    <a href=""> Residencies</a>
+                    <a href="">Our Values</a>
+                    <a href="">Contact us</a>
+                    <a href="">Get Started</a>
+                    <button className='button'>
+                        <a href="">Contact</a>
+                    </button>
+                </div>
+                <div className="menu-icon " onClick={()=>setMenuOpened ((prev)=>!prev
+                    )}>
+                <BiMenuAltRight size={30}/>
+            </div>
+            </div>
+            
+        </section>
+    )
+}
+
+export default Header
